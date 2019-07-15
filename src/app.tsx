15c983e77b2bdfe6bec82cc898b0e1e6a02cb38e
Eran Sakal <eran@sakalim.com>
2019-07-15 09:27:29 +0300

    layout chat components

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import { AppContent } from './components/app-content';
+
+export class App extends Component {
+  render() {
+    return <AppContent></AppContent>
+  }
+}
+
+export default App;

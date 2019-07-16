f43c511d14abd7a48c07d2c9f5e9a1215f912ae8
Eran Sakal <eran@sakalim.com>
2019-07-17 01:08:15 +0300

    remove future tasks

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,8 +1,5 @@
 import React, {Component} from 'react';
 import classes from './sidebar.module.css';
-import { UserProfile } from '../user-profile';
-import { ChannelList } from '../channel-list';
-import { ChannelCreate } from '../channel-create';
 import classnames from 'classnames';
 
 export interface SidebarProps {
@@ -13,9 +10,6 @@ export class Sidebar extends Component<SidebarProps> {
 	render() {
 		const { className } = this.props;
 		return <div className={classnames(classes.container, className)}>
-			<UserProfile className={classes.userProfile} />
-			<ChannelList className={classes.channelList} />
-			<ChannelCreate className={classes.channelCreate}/>
 		</div>;
 	}
 }

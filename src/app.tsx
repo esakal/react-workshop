575f12f4a3cd315dfebbe71b635b42f64893f386
Eran Sakal <eran@sakalim.com>
2019-07-16 18:43:10 +0300

    complete novice-3 task

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,52 @@
 import React, {Component} from 'react';
-import './app.css'
+import { ChatkitService } from './chatkit-service';
+import './app.css';
 
 export interface AppProps {
 
 }
 
 export interface AppState {
-
+    loading: boolean;
+    error: boolean;
+    fullName: string;
 }
 
 export class App extends Component<AppProps, AppState> {
-  render() {
-      return (
-          <div />
-      );
-  }
+    private chatkitService: ChatkitService = new ChatkitService();
+
+    state: AppState = {
+        loading: true,
+        error: false,
+        fullName: ''
+    }
+
+    componentDidMount(): void {
+        this.chatkitService.connect()
+            .then((result) => {
+                this.setState({
+                    fullName: result.user.name,
+                    loading: false,
+                });
+            })
+            .catch((error) => {
+                this.setState({
+                    error: true,
+                    loading: false,
+                });
+            });
+    }
+
+    render() {
+        const {loading, error, fullName} = this.state;
+        return (
+            <div>
+                {loading && <div>Loading...</div>}
+                {error && <div>Failed to connect...</div>}
+                {fullName && <div>Hello {fullName}</div>}
+            </div>
+        );
+    }
 }
 
 export default App;

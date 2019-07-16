bff7e900130be86af4fc80158754e0e52f922b68
Eran Sakal <eran@sakalim.com>
2019-07-17 01:23:10 +0300

    complete task

---
+++ src/components/app-content/app-content.tsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import classes from './app-content.module.css';
+
+export class AppContent extends Component {
+	render() {
+		return (
+		<div className={classes.container}>
+		</div>
+		);
+	}
+}
